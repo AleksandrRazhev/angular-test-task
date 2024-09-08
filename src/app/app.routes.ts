e002ffc91b61775fc053b8e9841b2a2a13444861
AlexandrRazhev <alexandrrazh@gmail.com>
2024-09-08 13:50:56 +0300

    project started

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { RegistrationPageComponent } from './pages/registration-page/registration-page.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { CallsPageComponent } from './pages/calls-page/calls-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: RegistrationPageComponent },
+  { path: 'main', component: MainPageComponent },
+  { path: 'calls', component: CallsPageComponent },
+];
---
+++ src/app/pages/calls-page/calls-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calls-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './calls-page.component.html',
+  styleUrl: './calls-page.component.css'
+})
+export class CallsPageComponent {
+
+}
