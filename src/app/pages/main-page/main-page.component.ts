51afeb875839c4ea601987b87e438c6e0d8ce466
AleksandrRazhev <alexandrrazh@gmail.com>
2024-09-11 13:20:20 +0300

    button text switching fixed

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 
 import { TimerService } from '../../services/timer.service';
 import { AudioService } from '../../services/audio.service';
+import { ITextButtons } from '../../Models/Buttons';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css',
 })
@@ -15,17 +17,35 @@ export class MainPageComponent {
     public timerService: TimerService,
     public audioService: AudioService
   ) {}
-  async start() {
+  textButtons: ITextButtons = {
+    topButton: 'Начать разговор',
+    bottomButton: 'Нет записей',
+  };
+  async onClickTopButton() {
     try {
       const mediaStream = await this.audioService.startStream();
       this.audioService.startRecord(mediaStream);
       this.timerService.start();
+      this.textButtons.topButton = 'Запись...';
+      this.textButtons.bottomButton = 'Окончить разговор';
     } catch (error) {
       console.error("I can't record", error);
     }
   }
-  async stop() {
-    this.audioService.stopRecord();
-    this.timerService.stop();
+  onClickBottomButton() {
+    if (this.audioService.isRecording) {
+      this.audioService.stopRecord();
+      this.timerService.stop();
+      this.textButtons.topButton = 'Начать разговор';
+      this.textButtons.bottomButton = 'Воспроизвести запись';
+    } else {
+      if (this.audioService.isPlaying) {
+        this.audioService.stopPlay();
+        this.textButtons.bottomButton = 'Воспроизвести запись';
+      } else {
+        this.audioService.statPlay();
+        this.textButtons.bottomButton = 'Остановить воспроизведение';
+      }
+    }
   }
 }
--- src/app/services/audio.service.ts
+++ src/app/services/audio.service.ts
@@ -5,24 +5,43 @@ import { Injectable } from '@angular/core';
 })
 export class AudioService {
   recorder: MediaRecorder | null = null;
-  blob: Blob[] = [];
+  private blob: BlobPart[] = [];
+  private audioUrl: string = '';
+  private audioElement: HTMLAudioElement = new Audio();
+  isRecording: boolean = false;
+  isPlaying: boolean = false;
   startStream(): Promise<MediaStream> {
     return navigator.mediaDevices.getUserMedia({ audio: true });
   }
   startRecord(mediaStream: MediaStream) {
+    if (this.audioUrl) URL.revokeObjectURL(this.audioUrl);
     this.recorder = new MediaRecorder(mediaStream);
     this.recorder.ondataavailable = (event) => {
-      this.blob.push(event.data);
+      this.blob = [event.data];
     };
     this.recorder.start();
+    this.isRecording = true;
   }
   stopRecord() {
+    this.isRecording = false;
     if (this.recorder) {
       this.recorder.stop();
-      const audioBlob = new Blob(this.blob, { type: 'audio/wav' });
-      const audioUrl = URL.createObjectURL(audioBlob);
-      const audio = new Audio(audioUrl);
-      audio.play();
     }
+  }
+  statPlay() {
+    if (this.isRecording) return;
+    const audioBlob = new Blob(this.blob, { type: 'audio/wav' });
+    this.audioUrl = URL.createObjectURL(audioBlob);
+    this.audioElement.src = this.audioUrl;
+    this.isPlaying = true;
+    this.audioElement.play();
+    this.audioElement.onended = () => {
+      this.isPlaying = false;
+    };
+  }
+  stopPlay() {
+    this.audioElement.pause();
+    this.audioElement.currentTime = 0;
+    this.isPlaying = false;
   }
 }
