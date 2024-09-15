2bc83889397f568a359e2b35e45e6463bd02c6db
AleksandrRazhev <alexandrrazh@gmail.com>
2024-09-15 20:12:04 +0300

    buttons styles fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from './components/header/header.component';
 import { ModalService } from './services/modal.service';
+import { UserService } from './services/user.service';
+import { CallsService } from './services/calls.service';
 
 @Component({
   standalone: true,
@@ -10,6 +12,7 @@ import { ModalService } from './services/modal.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   imports: [RouterOutlet, HeaderComponent],
+  providers: [UserService, CallsService],
 })
 export class AppComponent {
   constructor(public modalService: ModalService) {}
